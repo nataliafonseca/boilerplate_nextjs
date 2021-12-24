13e3102b6afb81a1544528e753a412f0f532ce64
Natália Fonseca <nataliabf@outlook.com>
2021-12-24 14:35:33 -0300

    chore: initialize project

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Boilerplate | NextJS</title>
+        <meta name="description" content="NextJS Boilerplate." />
+      </Head>
+
+      <h1>Boilerplate | NextJS</h1>
+    </div>
+  );
+};
+
+export default Home;
